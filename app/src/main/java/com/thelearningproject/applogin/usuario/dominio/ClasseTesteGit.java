76ececa115acd4bed0b3d767dff35c3d3fb6409e
Nícolas <nicolas.moura@ufrpe.br>
2017-07-25 17:21:11 -0300

    modificando teste

--- app/src/main/java/com/thelearningproject/applogin/usuario/dominio/ClasseTesteGit.java
+++ app/src/main/java/com/thelearningproject/applogin/usuario/dominio/ClasseTesteGit.java
@@ -2,5 +2,13 @@ package com.thelearningproject.applogin.usuario.dominio;
 
 public class ClasseTesteGit {
     //TesteGitGommit
+    private  String teste = "TEEESTE";
 
+    public void modificaTeste (String modificador){
+        this.teste += modificador;
+    }
+
+    public String getTeste() {
+        return teste;
+    }
 }
