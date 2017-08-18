bb684ffba38b26b84579819d836e626126ae44e2
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2017-08-18 00:23:26 -0300

    Finalização activity perfil

--- app/src/main/java/com/thelearningproject/applogin/perfil/gui/PerfilActivity.java
+++ app/src/main/java/com/thelearningproject/applogin/perfil/gui/PerfilActivity.java
@@ -1,7 +1,9 @@
 package com.thelearningproject.applogin.perfil.gui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.thelearningproject.applogin.R;
@@ -10,11 +12,8 @@ import com.thelearningproject.applogin.perfil.negocio.PerfilServices;
 
 public class PerfilActivity extends AppCompatActivity {
     private Perfil perfilAtual;
-    private TextView tvNomePerfil;
-    private TextView tvDescricaoPerfil;
-//    private PerfilServices perfilServices = PerfilServices.getInstancia(activity);
 
-//    public PerfilActivity() {
+    //    public PerfilActivity() {
 //        this.perfilAtual = (Perfil) getIntent().getSerializableExtra("Perfil");
 //    }
     public PerfilActivity(Perfil p) {
@@ -26,16 +25,39 @@ public class PerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         setTitle(perfilAtual.getPessoa().getNome());
+        PerfilServices perfilServices = PerfilServices.getInstancia(PerfilActivity.this);
 
-        tvNomePerfil = (TextView) findViewById(R.id.nomeUsuarioID);
-        tvDescricaoPerfil = (TextView) findViewById(R.id.descricaoUsuarioID);
+        TextView tvNomePerfil = (TextView) findViewById(R.id.nomeUsuarioID);
+        TextView tvDescricaoPerfil = (TextView) findViewById(R.id.descricaoUsuarioID);
 
         tvNomePerfil.setText(perfilAtual.getPessoa().getNome());
         tvDescricaoPerfil.setText(perfilAtual.getDescricao());
 
-/*        PerfilServices perfilServices;
         String[] listaHabilidades = {perfilServices.retornaStringListaHabilidades(perfilAtual.getId())};
         String[] listaNecessidades = {perfilServices.retornaStringListaNecessidades(perfilAtual.getId())};
-        String[] listaOpcoes = {getString(R.string.iniciarChat)};*/
+        String[] listaOpcoes = {getString(R.string.iniciarChat)};
+
+        ListView lvHabilidades = (ListView) findViewById(R.id.listaPerfilHabilidades);
+        ListView lvNecessidades = (ListView) findViewById(R.id.listaPerfilNecessidades);
+        ListView lvOpcoes = (ListView) findViewById(R.id.listaPerfilOpcoes);
+
+        ArrayAdapter<String> adapterOpcoesHabilidades = new ArrayAdapter<>(
+                PerfilActivity.this,
+                android.R.layout.simple_list_item_1,
+                listaHabilidades);
+        ArrayAdapter<String> adapterOpcoesNecessidades = new ArrayAdapter<>(
+                PerfilActivity.this,
+                android.R.layout.simple_list_item_1,
+                listaNecessidades);
+        ArrayAdapter<String> adapterOpcoesOutras = new ArrayAdapter<>(
+                PerfilActivity.this,
+                android.R.layout.simple_list_item_1,
+                listaOpcoes);
+
+        lvHabilidades.setAdapter(adapterOpcoesHabilidades);
+        lvNecessidades.setAdapter(adapterOpcoesNecessidades);
+        lvOpcoes.setAdapter(adapterOpcoesOutras);
+
+        lvOpcoes.setAlpha((float) 0.75);
     }
 }
