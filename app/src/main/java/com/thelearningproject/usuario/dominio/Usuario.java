ddd1d54d6f45bc0045ccb36578ee1430bf011d6f
LUCIANO <nanotrigueiro@hotmail.com>
2017-08-27 04:39:02 -0300

    Criação JavaDoc Classe usuário

--- app/src/main/java/com/thelearningproject/usuario/dominio/Usuario.java
+++ app/src/main/java/com/thelearningproject/usuario/dominio/Usuario.java
@@ -3,43 +3,82 @@ package com.thelearningproject.usuario.dominio;
 import com.thelearningproject.infraestrutura.utils.Status;
 
 /**
- * Criado por Ebony Marques on 17/07/2017.
+ * The type Usuario.
  */
-
 public class Usuario {
     private int id;
     private String email;
     private String senha;
     private Status status;
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email.
+     *
+     * @param novoEmail the novo email
+     */
     public void setEmail(String novoEmail) {
         this.email = novoEmail;
     }
 
+    /**
+     * Gets senha.
+     *
+     * @return the senha
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * Gets status.
+     *
+     * @return the status
+     */
     public Status getStatus() {
         return status;
     }
 
+    /**
+     * Sets status.
+     *
+     * @param novoStatus the novo status
+     */
     public void setStatus(Status novoStatus) {
         this.status = novoStatus;
     }
 
+    /**
+     * Sets senha.
+     *
+     * @param novaSenha the nova senha
+     */
     public void setSenha(String novaSenha) {
         this.senha = novaSenha;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param novoId the novo id
+     */
     public void setId(int novoId) {
         this.id = novoId;
     }
